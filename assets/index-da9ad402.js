883bf0f9b6a52cda5cb4e59bc87be05cd3c8c832
bijinfeng <bijinfeng@users.noreply.github.com>
2023-02-08 09:16:33 +0000

    deploy: cc87cfd54a9cb91341f5b9ad6d433b064e3fda30

--- assets/index-da9ad402.js
+++
@@ -1,1 +0,0 @@
-import{a as t}from"./index-401e96bf.js";const o=()=>t("div",{className:" text-secondary text-lg mt-[35vh] font-bold text-center",children:"You Are Lost  :)"});export{o as default};
---
+++ assets/useScrollTop-55a76156.js
@@ -0,0 +1,1 @@
+import{u as o}from"./useIsomorphicEffect-6f95aa50.js";const e=()=>{o(()=>{document.getElementById("root").scrollTo({top:0})},[])};export{e as u};
