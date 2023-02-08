a0a1e9849ce267fd03be5d86437b8c40b52f344d
bijinfeng <bijinfeng@bytedance.com>
2023-02-08 17:16:45 +0800

    build: fix build base error

--- vite.config.ts
+++ vite.config.ts
@@ -7,7 +7,7 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default ({ mode }: { mode: string }) => {
   const ENV = { ...process.env, ...loadEnv(mode, process.cwd()) };
-  const isDev = ENV.NODE_ENV !== "development";
+  const isDev = ENV.NODE_ENV === "development";
 
   return defineConfig({
     base: isDev ? "/" : "/music-motion-x/",
