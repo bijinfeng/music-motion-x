f3f0d88d2337dfc7631b80015b0055268641215a
bijinfeng <bijinfeng@users.noreply.github.com>
2023-02-13 13:54:16 +0000

    deploy: 2d17bbfb98b48820be4feb4fa9bf2f9b77f2396c

--- assets/useScrollTop-09fc1e57.js
+++
@@ -1,1 +0,0 @@
-import{n as o}from"./index-fb9d91aa.js";const e=()=>{o(()=>{document.getElementById("root").scrollTo({top:0})},[])};export{e as u};
---
+++ assets/useScrollTop-f1d24d8b.js
@@ -0,0 +1,1 @@
+import{m as o}from"./index-db6e8751.js";const e=()=>{o(()=>{document.getElementById("root").scrollTo({top:0})},[])};export{e as u};
